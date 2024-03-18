6f3ac3f0d0ea38a4b154d74cdcbf35c2a9f9c825
jlcastillog <98542306+jlcastillog@users.noreply.github.com>
2024-03-18 18:36:47 +0100

    feat: Add class 2

---
+++ Javascript/Fundamentos/clase-2/clase-variables.js
@@ -0,0 +1,10 @@
+//El nombre de las variables deben de ser descriptivas
+
+//LO NO PERMITIDO
+let c = "Prueba"
+let mpv = 'Mi primera variable'
+let mpVariable = 'Mi primera variable'
+
+//LO PERMITIDO
+let miPrimeraVariable = 'Mi primera variable'
+console.log(miPrimeraVariable)
