c740981963bf5e3892ef9fc931c6cb0de4385060
jlcastillog <98542306+jlcastillog@users.noreply.github.com>
2024-03-20 17:06:48 +0100

    feat: Add class 5

---
+++ Javascript/Fundamentos/clase-5/clase-number.js
@@ -0,0 +1,20 @@
+// 1. Tipo entero y decimal
+const entero = 42
+const decimal = 3.14
+console.log(typeof(entero))
+console.log(typeof(decimal))
+
+// 2. Notación cientifica
+const cientifico = 5e3
+
+// 3. Infinitos y NaN
+const infinito = Infinity
+const noEsNumero = NaN
+
+// Operaciones avanzadas
+const raizCuadrada = Math.sqrt(16)
+const valorAbsoluto = Math.abs(-7)
+const numeroRandom = Math.random()
+console.log(raizCuadrada)
+console.log(valorAbsoluto)
+console.log(numeroRandom)
