ced5e48a34f1ad89c1f21d6dabaed4b160ab98d0
jlcastillog <98542306+jlcastillog@users.noreply.github.com>
2024-04-16 19:05:51 +0200

    Add: Create xUnit project and first test method

---
+++ UnitTestingDotnet/StringManipulation.Tests/StringOperationsTest.cs
@@ -0,0 +1,15 @@
+﻿namespace StringManipulation.Tests
+{
+    public class StringOperationsTest
+    {
+        [Fact]
+        public void ConcatenateString()
+        {
+            var stringOperations = new StringOperations();
+
+            var result = stringOperations.ConcatenateStrings("Hello", "Platzi");
+
+            Assert.Equal("Hello Platzi", result);  
+        }
+    }
+}
