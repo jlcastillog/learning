f5c5bde164dbe84735800b5033c52fbe88437737
jlcastillog <98542306+jlcastillog@users.noreply.github.com>
2024-04-17 18:50:35 +0200

    Feat: Introduction to different assert types

--- UnitTestingDotnet/StringManipulation.Tests/StringOperationsTest.cs
+++ UnitTestingDotnet/StringManipulation.Tests/StringOperationsTest.cs
@@ -2,14 +2,109 @@
 {
     public class StringOperationsTest
     {
-        [Fact]
+        [Fact(Skip = "Deprecated")]
         public void ConcatenateString()
         {
+            // Arrange
             var stringOperations = new StringOperations();
 
+            // Act
             var result = stringOperations.ConcatenateStrings("Hello", "Platzi");
 
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
             Assert.Equal("Hello Platzi", result);  
+        }
+
+        [Fact]
+        public void IsPalindrome_True()
+        {
+            // Arrange
+            var stringOperations = new StringOperations();
+
+            // Act
+            var result = stringOperations.IsPalindrome("ama");
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsPalindrome_False()
+        {
+            // Arrange
+            var stringOperations = new StringOperations();
+
+            // Act
+            var result = stringOperations.IsPalindrome("hello");
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void QuantintyInWords()
+        {
+            // Arrange
+            var stringOperations = new StringOperations();
+
+            // Act
+            var result = stringOperations.QuantintyInWords("coche", 7);
+
+            // Assert
+            Assert.StartsWith("siete", result);
+            Assert.Contains("coche", result);
+            Assert.Equal("siete coches", result);
+        }
+
+        [Fact]
+        public void GetStringLength_Exception()
+        {
+            // Arrange
+            var stringOperations = new StringOperations();
+
+            // Assert
+            Assert.ThrowsAny<ArgumentNullException>(() => stringOperations.GetStringLength(null));
+        }
+
+        [Fact]
+        public void GetStringLength() 
+        {
+            // Arrange
+            var stringOperations = new StringOperations();
+
+            // Act
+            var result = stringOperations.GetStringLength("car");
+
+            // Assert
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void TruncateString_Exception()
+        {
+            // Arrange
+            var stringOperations = new StringOperations();
+
+            // Assert
+            Assert.ThrowsAny<ArgumentOutOfRangeException>(() => stringOperations.TruncateString("car", 0));
+        }
+
+        [Theory]
+        [InlineData("V",5)]
+        [InlineData("X", 10)]
+        [InlineData("XXII", 22)]
+        public void FromRomanToNumber(string romanNumber, int expected)
+        {
+            // Arrange
+            var stringOperations = new StringOperations();
+
+            // Act
+            var result = stringOperations.FromRomanToNumber(romanNumber);
+
+            //Assert
+            Assert.Equal(expected, result);
         }
     }
 }
