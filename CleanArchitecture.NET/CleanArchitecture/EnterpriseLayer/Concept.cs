619ebe0456c2822d6666c280ca46a2fb018d547a
jlcastillog <98542306+jlcastillog@users.noreply.github.com>
2024-09-22 17:17:07 +0200

    feat: Add entities Concept and Sale

---
+++ CleanArchitecture.NET/CleanArchitecture/EnterpriseLayer/Concept.cs
@@ -0,0 +1,21 @@
+﻿namespace EnterpriseLayer
+{
+    public class Concept
+    {
+        public int IdBeer { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal Price { get; set; }
+
+        public Concept(int quantity, int idBeer, decimal unitPrice)
+        {
+            Quantity = quantity;
+            IdBeer = idBeer;    
+            UnitPrice = unitPrice; 
+            Price = GetTotalPrice();
+        }
+
+        private decimal GetTotalPrice()
+            => Quantity * UnitPrice;
+    }
+}
