6ecc89d4cace9a41cf9eb57b27342497ff530d7d
jlcastillog <98542306+jlcastillog@users.noreply.github.com>
2024-09-22 19:39:52 +0200

    feat: Add models SaleModel and ConceptModel

---
+++ CleanArchitecture.NET/CleanArchitecture/InterfaceAdapters-Models/ConceptModel.cs
@@ -0,0 +1,11 @@
+﻿namespace InterfaceAdapters_Models
+{
+    public class ConceptModel
+    {
+        public string Id { get; set; }
+        public int IdBeer { get; set; }
+        public int IdSale { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
---
+++ CleanArchitecture.NET/CleanArchitecture/InterfaceAdapters-Models/SaleModel.cs
@@ -0,0 +1,10 @@
+﻿namespace InterfaceAdapters_Models
+{
+    public class SaleModel
+    {
+        public int Id { get; }
+        public DateTime CreationDate { get; set; }
+        public decimal Total { get; set; }
+        public List<ConceptModel> Concepts { get; set; }
+    }
+}
