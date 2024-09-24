f215d682bb266349a57bbe40952ae1780c9dad64
jlcastillog <98542306+jlcastillog@users.noreply.github.com>
2024-09-24 16:27:08 +0200

    fix: Remove unnecessary mapper in "GetSalesUseCase"

--- CleanArchitecture.NET/CleanArchitecture/ApplicationLayer/GetSalesUseCase.cs
+++ CleanArchitecture.NET/CleanArchitecture/ApplicationLayer/GetSalesUseCase.cs
@@ -3,15 +3,13 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ApplicationLayer
 {
-    public class GetSalesUseCase<TDto>
+    public class GetSalesUseCase
     {
         private readonly IRepository<SaleEntity> _saleRepository;
-        private readonly IMapper<TDto, SaleEntity> _mapper;
 
-        public GetSalesUseCase(IRepository<SaleEntity> saleRepository, IMapper<TDto, SaleEntity> mapper)
+        public GetSalesUseCase(IRepository<SaleEntity> saleRepository)
         {
             _saleRepository = saleRepository;
-            _mapper = mapper;
         }
 
         public async Task<IEnumerable<SaleEntity>> ExecuteAsync()
