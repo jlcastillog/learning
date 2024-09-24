6c2feaa301ec4c52db554bbf8ac7fbf5b2f1bb77
jlcastillog <98542306+jlcastillog@users.noreply.github.com>
2024-09-24 16:25:19 +0200

    feat: Add use case "GetSalesUseCase"

---
+++ CleanArchitecture.NET/CleanArchitecture/ApplicationLayer/GetSalesUseCase.cs
@@ -0,0 +1,22 @@
+﻿using EnterpriseLayer;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApplicationLayer
+{
+    public class GetSalesUseCase<TDto>
+    {
+        private readonly IRepository<SaleEntity> _saleRepository;
+        private readonly IMapper<TDto, SaleEntity> _mapper;
+
+        public GetSalesUseCase(IRepository<SaleEntity> saleRepository, IMapper<TDto, SaleEntity> mapper)
+        {
+            _saleRepository = saleRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<SaleEntity>> ExecuteAsync()
+        {
+            return await _saleRepository.GetAllAsync();
+        }
+    }
+}
