d6e4344f80a9fd05018f144cf2df35432f98a995
jlcastillog <98542306+jlcastillog@users.noreply.github.com>
2024-11-03 15:06:09 +0100

    feat: Increase size to XCircleIcon

--- React-Vite-TailwindCSS/react-course/src/Components/ProductDetail/index.jsx
+++ React-Vite-TailwindCSS/react-course/src/Components/ProductDetail/index.jsx
@@ -6,7 +6,7 @@ const ProductDetail = () => {
     <aside className="product-detail flex flex-col fixed right-0 border border-black rounded-lg bg-white">
       <div className="flex justify-between items-center p-5">
         <h2 className="font-medium text-xl">Detail</h2>
-        <XCircleIcon className="size-6" />
+        <XCircleIcon className="size-7" />
       </div>
     </aside>
   );
