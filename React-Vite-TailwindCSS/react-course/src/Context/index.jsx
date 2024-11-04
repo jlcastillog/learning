83e33d91eb900c8d251fb3d4a3276f1e0b02ddfb
jlcastillog <98542306+jlcastillog@users.noreply.github.com>
2024-11-04 18:10:52 +0100

    feat: Add comment documentation

--- React-Vite-TailwindCSS/react-course/src/Context/index.jsx
+++ React-Vite-TailwindCSS/react-course/src/Context/index.jsx
@@ -5,18 +5,23 @@ export const ShoppingCartContext = createContext();
 
 export  const ShoppingCartProvider = ({ children }) => {
 
+  // Products counter in cart
   const [count, setCount] = useState(0);
 
+  // Product detail
   const [isProductDetailVisible, setIsProductDetailVisible] = useState(false);
   const showProductDetail = () => setIsProductDetailVisible(true)
   const hideProductDetail = () => setIsProductDetailVisible(false)
 
+  // My order detail
   const [isMyOrderDetailVisible, setIsMyOrderDetailVisible] = useState(false);
   const showMyOrderDetail = () => setIsMyOrderDetailVisible(true)
   const hideMyOrderDetail = () => setIsMyOrderDetailVisible(false)
 
+  // Selected product
   const [selectedProduct, setSelectedProduct] = useState({})
 
+  // Cart products
   const [cartProducts, setCartProducts] = useState([])
 
   return (
