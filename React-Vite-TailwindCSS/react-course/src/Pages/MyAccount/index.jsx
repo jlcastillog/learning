6d65ee8c9657c4da5a5b80bf4276e2d196d432f7
jlcastillog <98542306+jlcastillog@users.noreply.github.com>
2024-11-15 18:22:03 +0100

    feat: Layout page MyAccount

--- React-Vite-TailwindCSS/react-course/src/Pages/MyAccount/index.jsx
+++ React-Vite-TailwindCSS/react-course/src/Pages/MyAccount/index.jsx
@@ -3,7 +3,26 @@ import { Layout } from "../../Components/Layout";
 function MyAccount() {
   return (
     <>
-      <Layout>MyAccount</Layout>
+      <Layout>
+        <div className="flex item-center justify-center relative w-80 mb-4">
+          <h1 className="font-medium text-xl">My account</h1>
+        </div>
+        <div className="flex flex-col item-center justify-center w-80 mb-4">
+          <p>
+            <span className="font-light">Name: </span>
+            <span className="font-medium">jlcgalvez</span>
+          </p>
+          <p>
+            <span className="font-light">Email: </span>
+            <span className="font-medium">jlcgalvez@gmail.com</span>
+          </p>
+        </div>
+        <div className="flex item-center justify-center w-80 mb-4">
+          <button className="w-full py-3 bg-white text-black border border-black rounded-lg">
+            Edit
+          </button>
+        </div>
+      </Layout>
     </>
   );
 }
