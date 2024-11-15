2c834459b81ab4b9a9dea3fc676c51a5e2311071
jlcastillog <98542306+jlcastillog@users.noreply.github.com>
2024-11-15 18:07:06 +0100

    feat: Layout page Sign-in

--- React-Vite-TailwindCSS/react-course/src/Pages/SignIn/index.jsx
+++ React-Vite-TailwindCSS/react-course/src/Pages/SignIn/index.jsx
@@ -3,7 +3,34 @@ import { Layout } from "../../Components/Layout";
 function SignIn() {
   return (
     <>
-      <Layout>SignIn</Layout>
+      <Layout>
+        <div className="flex item-center justify-center relative w-80 mb-4">
+          <h1 className="font-medium text-xl">Wellcome</h1>
+        </div>
+        <div className="flex flex-col item-center justify-center w-80 mb-4">
+          <p>
+            <span className="font-light">Email: </span>
+            <span className="font-medium">jlcgalvez@gmail.com</span>
+          </p>
+          <p>
+            <span className="font-light">Password: </span>
+            <span className="font-medium">123456</span>
+          </p>
+        </div>
+        <div className="flex item-center justify-center w-80 mb-4">
+          <button className="w-full py-3 bg-black text-white rounded-lg">
+            Log in
+          </button>
+        </div>
+        <div className="flex item-center justify-center w-80 mb-6">
+          <p className="font-medium text-sm underline underline-offset-4 cursor-pointer">Forgot my password</p>
+        </div>
+        <div className="flex item-center justify-center w-80 mb-4">
+          <button className="w-full py-3 bg-white text-gray-400 border border-gray-400 rounded-lg">
+            Sign up
+          </button>
+        </div>
+      </Layout>
     </>
   );
 }
