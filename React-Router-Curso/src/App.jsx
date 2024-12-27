e4b5660ff4317b8c8feb4cb904063899b0062cbf
jlcastillog <98542306+jlcastillog@users.noreply.github.com>
2024-12-27 18:09:52 +0100

    feat: Pages and main navigation

--- React-Router-Curso/src/App.jsx
+++ React-Router-Curso/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Menu } from './Components/Menu';
+import { HomePage } from './Pages/HomePage';
+import { BlogPage } from './Pages/BlogPage';
+import { ProfilePage } from './Pages/ProfilePage';
+import { BlogPost } from './Components/BlogPost';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <HashRouter>
+        <Menu />
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/blog/:slug" element={<BlogPost />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="*" element={<p>Not found</p>} />
+        </Routes>
+      </HashRouter>
     </>
-  )
+  );
 }
 
 export default App
---
+++ React-Router-Curso/src/Components/Menu/index.jsx
@@ -0,0 +1,38 @@
+import { NavLink } from 'react-router-dom';
+
+function Menu() {
+  return (
+    <nav>
+      <ul>
+        {routes.map(route => (
+          <li key={route.to}>
+            <NavLink
+              style={({ isActive }) => ({
+                color: isActive ? 'red' : 'blue',
+              })}
+              to={route.to}
+            >
+              {route.text}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+const routes = [];
+routes.push({
+  to: '/',
+  text: 'Home',
+});
+routes.push({
+  to: '/blog',
+  text: 'Blog',
+});
+routes.push({
+  to: '/profile',
+  text: 'Profile',
+});
+
+export { Menu };
