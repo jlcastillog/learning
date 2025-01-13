e6225e1759998b2694ae61954a82c97aee6ff027
jlcastillog <98542306+jlcastillog@users.noreply.github.com>
2025-01-13 18:39:49 +0100

    feat: implement events and transactions

--- State-Machines-React-curso/react-state-machine/src/App.jsx
+++ State-Machines-React-curso/react-state-machine/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import BaseLayout from './components/BaseLayout'
+import BaseLayout from './containers/BaseLayout'
 import './App.css'
 
 function App() {
---
+++ State-Machines-React-curso/react-state-machine/src/containers/BaseLayout.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useMachine } from '@xstate/react';
+import { Nav } from '../components/Nav';
+import { StepsLayout } from './StepsLayout';
+import bookingMachine from '../machines/bookingMachine';
+import './BaseLayout.css';
+
+
+function BaseLayout() {
+  const [state, send] = useMachine(bookingMachine);
+
+  console.log("Nuestra maquina", state.value);
+
+  return (
+    <div className='BaseLayout'>
+      <Nav state={state} send={send} />
+      <StepsLayout state={state} send={send}/>
+    </div>
+  );
+}
+
+export default BaseLayout;
