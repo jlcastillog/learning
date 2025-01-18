7585ccce63c56c3bcb543291e99016282c658677
jlcastillog <98542306+jlcastillog@users.noreply.github.com>
2025-01-18 10:28:05 +0100

    feat: fetch countries and validations

--- State-Machines-React-curso/react-state-machine/src/machines/bookingMachine.js
+++ State-Machines-React-curso/react-state-machine/src/machines/bookingMachine.js
@@ -1,61 +1,120 @@
-import { createMachine, assign } from "xstate";
+import { createMachine, assign, fromPromise } from "xstate";
+import { fetchCountries } from "../utils/api";
 
-const bookingMachine = createMachine({
-  id: "buy plane tickets",
-  initial: "initial",
-  context: {
-    passengers: [],
-    selectedCountry: "null",
-  },
+const fillCountries = {
+  initial: "loading",
   states: {
-    initial: {
-      on: {
-        START: {
-          target: "search",
-        },
-      },
-    },
-    search: {
-      on: {
-        CONTINUE: {
-          target: "passengers",
-          actions: assign(({ context, event }) => {
-            context.selectedCountry = event.selectedCountry;
+    loading: {
+      invoke: {
+        id: "getCountries",
+        src: fromPromise(() => fetchCountries()),
+        onDone: {
+          target: "success",
+          actions: assign({
+            countries: ({ event }) => event.output,
           }),
         },
-        CANCEL: {
-          target: "initial",
-          actions: assign(({context}) => { 
-            context.selectedCountry = "null";
-            context.passengers = [];  
+        onError: {
+          target: "failure",
+          actions: assign({
+            error: "Fallo el request",
           }),
         },
       },
     },
-    tickets: {
+    success: {},
+    failure: {
       on: {
-        FINISH: "initial",
-      },
-    },
-    passengers: {
-      on: {
-        DONE: "tickets",
-        CANCEL: {
-          target: "initial",
-          actions: assign(({context}) => { 
-            context.selectedCountry = "null";
-            context.passengers = [];  
-          }),
-        },
-        ADD: {
-          target: "passengers",
-          actions: assign(({ context, event }) => {
-            context.passengers.push(event.passenger);
-          }),
+        RETRY: {
+          target: "loading",
         },
       },
     },
   },
-});
+};
+
+const bookingMachine = createMachine(
+  {
+    id: "buy plane tickets",
+    initial: "initial",
+    context: {
+      passengers: [],
+      selectedCountry: "null",
+      countries: [],
+      error: "",
+    },
+    states: {
+      initial: {
+        on: {
+          START: {
+            target: "search",
+          },
+        },
+      },
+      search: {
+        on: {
+          CONTINUE: {
+            target: "passengers",
+            actions: assign(({ context, event }) => {
+              context.selectedCountry = event.selectedCountry;
+            }),
+          },
+          CANCEL: {
+            target: "initial",
+            actions: assign(({ context }) => {
+              context.selectedCountry = "null";
+              context.passengers = [];
+            }),
+          },
+        },
+        ...fillCountries,
+      },
+      tickets: {
+        after: {
+          5000: {
+            target: "initial",
+            actions: "cleanContext",
+          },
+        },
+        on: {
+          FINISH: "initial",
+        },
+      },
+      passengers: {
+        on: {
+          DONE: {
+            target: "tickets",
+            guard: "moreThanOnePassenger",
+          },
+          CANCEL: {
+            target: "initial",
+            actions: "cleanContext",
+          },
+          ADD: {
+            target: "passengers",
+            actions: assign(({ context, event }) => {
+              context.passengers.push(event.passenger);
+            }),
+          },
+        },
+      },
+    },
+  },
+  {
+    actions: {
+      cleanContext: assign(({ context }) => {
+        context.selectedCountry = "null";
+        context.passengers = [];
+        context.error = "";
+        context.countries = [];
+      }),
+    },
+    guards: {
+      moreThanOnePassenger: ({ context }) => {
+        return context.passengers.length > 0;
+      },
+    },
+  }
+);
 
 export default bookingMachine;
